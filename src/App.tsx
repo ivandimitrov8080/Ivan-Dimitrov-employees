fbb5156c9a1f907f5984cfd818c26c91a96d4809
Ivan Dimitrov <ivan@ivandimitrov.co.uk>
2023-03-13 01:43:33 +0200

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ const App: React.FC<{}> = () => {
   }
 
   return (
-    <div className='ctnr'>
+    <div className='ctnr dark'>
       {loading ? <Spinner /> : ui()}
     </div>
   )
--- src/components/EmployeeTable.tsx
+++ src/components/EmployeeTable.tsx
@@ -7,19 +7,19 @@ const EmployeeTable: React.FC<{ employeeRelationships: EmployeeRelationship[] | 
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+            <th scope="col" className="th1">
               Employee ID #1
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="th2">
               Employee ID #2
             </th>
-            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+            <th scope="col" className="th1">
               Project ID
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="th2">
               Days Worked On Project
             </th>
-            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+            <th scope="col" className="th1">
               Total Days Worked
             </th>
           </tr>
@@ -28,11 +28,11 @@ const EmployeeTable: React.FC<{ employeeRelationships: EmployeeRelationship[] | 
           {employeeRelationships ?
             employeeRelationships.map(r => (
               <tr className="border-b border-gray-200 dark:border-gray-700">
-                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">{r.eid1}</td>
-                <td className="px-6 py-4">{r.eid2}</td>
-                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">{r.projects.map(p => (<div>{p.pid}<br /></div>))}</td>
-                <td className="px-6 py-4">{r.projects.map(p => (<div>{p.daysWorked}<br /></div>))}</td>
-                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">{r.totalDaysWorked}</td>
+                <td className="td1">{r.eid1}</td>
+                <td className="td2">{r.eid2}</td>
+                <td className="td1">{r.projects.map(p => (<div>{p.pid}<br /></div>))}</td>
+                <td className="td2">{r.projects.map(p => (<div>{p.daysWorked}<br /></div>))}</td>
+                <td className="td1">{r.totalDaysWorked}</td>
               </tr>
             )) :
             null}
